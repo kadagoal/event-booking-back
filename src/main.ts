b2cc79f9c8c7d9c6a22c3266195a4d1ebd27dcf6
holman sneyder <kadagoal@gmail.com>
2025-04-24 15:43:44 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
